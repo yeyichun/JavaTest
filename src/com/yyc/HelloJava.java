510505fcbc1d0b6b4acf77560abf10acb16d6b5a
yeyichun <1554585190@qq.com>
2017-02-28 18:15:10 +0800

    JavaTest1 Project for Git Test.

---
+++ src/com/yyc/HelloJava.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.yyc;
+
+/**
+ * @author Administrator
+ *
+ */
+public class HelloJava {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        System.out.println("Hello Java.");
+	}
+
+}
