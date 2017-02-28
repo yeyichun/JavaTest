ce1409f243fc72afb2371e7842b0aeb297812163
yeyichun <1554585190@qq.com>
2017-02-28 18:22:17 +0800

    add 123

--- src/com/yyc/HelloJava.java
+++ src/com/yyc/HelloJava.java
@@ -14,7 +14,7 @@ public class HelloJava {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-        System.out.println("Hello Java.");
+        System.out.println("Hello Java 123.");
 	}
 
 }
