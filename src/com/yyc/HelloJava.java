fdd690e3cc923f770cef916869b6b28a17d13826
yeyichun <1554585190@qq.com>
2017-02-28 18:28:23 +0800

    delete 123 and add Python

--- src/com/yyc/HelloJava.java
+++ src/com/yyc/HelloJava.java
@@ -14,7 +14,7 @@ public class HelloJava {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-        System.out.println("Hello Java 123.");
+        System.out.println("Hello Java and Python.");
 	}
 
 }
